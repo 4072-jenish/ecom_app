846933cb782302f622a5d5257acc94764e2ae50c
4072-jenish <150036720+4072-jenish@users.noreply.github.com>
2024-03-13 18:01:41 +0530

    home page.dart

--- lib/home page.dart
+++ lib/home page.dart
@@ -1,6 +1,6 @@
-// ignore_for_file: unused_local_variable
-
+import 'package:ecom_app/globla.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class homepage extends StatelessWidget {
   const homepage({super.key});
@@ -20,75 +20,85 @@ class homepage extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        margin: const EdgeInsets.all(10),
-        height: 330,
-        width: 200,
-        decoration: const BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, 5),
-              blurRadius: 5,
-              spreadRadius: -2,
-              color: Colors.grey,
-            )
-          ],
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-        ),
-        child: Column(
+      body: Row(
           children: [
-            Expanded(
-                flex: 2,
-                child: Container(
-                  color: Colors.orange,
-                )),
-            Expanded(
-                flex: 1,
-                child: Container(
-                  padding: const EdgeInsets.all(5),
+        ...allproducts.map(
+          (e) => Container(
+            margin: const EdgeInsets.all(10),
+            height: 330,
+            width: 200,
+            decoration: const BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 5),
+                  blurRadius: 5,
+                  spreadRadius: -2,
+                  color: Colors.grey,
+                )
+              ],
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Column(
+              children: [
+                Expanded(flex: 2,
+                 child: Container(
                   decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(20),
-                        bottomRight: Radius.circular(20),
-                      )),
-                  child: const Align(
-                    alignment: Alignment.topLeft,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          "i phone 9",
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w900,
-                          ),
-                        ),
-                        Text(
-                          "549 ",
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w900,
-                          ),
-                        ),
-                        Text(
-                          "5 star ",
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w900,
-                          ),
-                        ),
-                      ],
-                    ),
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      topLeft: Radius.circular(20),
+                    ),   
                   ),
-                )),
-          ],
+                 )),
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                                                                bottomLeft: Radius.circular(20),
+                            bottomRight: Radius.circular(20),
+                          )),
+                      child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              "${e['title']}",
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                            Text(
+                              "${e['price']}",
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                            RatingBarIndicator(
+                                itemCount: 5,
+                                itemSize: 25,
+                                itemBuilder: (context, i) {
+                                  return const Icon(
+                                    Icons.star,
+                                    color: Colors.yellow,
+                                  );
+                                }),
+                          ],
+                        ),
+                      ),
+                    )),
+              ],
+            ),
+          ),
         ),
-      ),
+      ].toList()),
     );
   }
 }
